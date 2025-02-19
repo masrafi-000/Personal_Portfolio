e103f0691c3db16a7eab53403804e5c2a934520f
masrafi-000 <smmasrafi000@gmail.com>
2025-02-19 19:12:00 +0600

    FooterChanged

--- client/src/sections/Footer.jsx
+++ client/src/sections/Footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Footer = () => {
   return (
     <div className="bg-orange-100 text-sm text-secondary text-center p-5">
-     <span >&copy; Copyright 2025</span>  - S M Masrafi | Created By S M Masrafi
+      <span> Copyright &copy;-2025</span> | S M Masrafi | All Rights Reserved!
     </div>
   );
 };
